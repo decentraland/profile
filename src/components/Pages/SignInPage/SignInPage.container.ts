9dce71decef64e7178e9bc36766e13347bb6ca81
Florencia Barreto <florbarrlad@gmail.com>
2023-07-05 19:26:58 +0200

    feat: added test

--- src/components/Pages/SignInPage/SignInPage.container.ts
+++ src/components/Pages/SignInPage/SignInPage.container.ts
@@ -1,12 +1,11 @@
-import { connect } from "react-redux";
-import { isConnected } from "decentraland-dapps/dist/modules/wallet/selectors";
-
-import { RootState } from "../../../modules/reducer";
-import { MapStateProps } from "./SignInPage.types";
-import SignInPage from "./SignInPage";
+import { connect } from 'react-redux'
+import { isConnected } from 'decentraland-dapps/dist/modules/wallet/selectors'
+import { RootState } from '../../../modules/reducer'
+import SignInPage from './SignInPage'
+import { MapStateProps } from './SignInPage.types'
 
 const mapState = (state: RootState): MapStateProps => ({
-  isConnected: isConnected(state),
-});
+  isConnected: isConnected(state)
+})
 
-export default connect(mapState)(SignInPage);
+export default connect(mapState)(SignInPage)
