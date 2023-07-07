43f2f60faabce92a72ea21812fa12e65442284d2
Florencia Barreto <florbarrlad@gmail.com>
2023-07-07 14:54:07 +0200

    feat: added profile styles

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,20 @@
+import { Icon } from 'semantic-ui-react'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
+import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { WearablePreview } from 'decentraland-ui/dist/components/WearablePreview/WearablePreview'
+
+import styles from './Avatar.module.css'
+
+const Avatar = () => {
+  return (
+    <div className={styles.Avatar}>
+      <WearablePreview disableBackground={true} profile="0xd4fEC88A49EB514e9347eC655D0481D8483a9AE0" />
+      <Button secondary fluid>
+        <Icon name="edit" />
+        {t('navigation.places')}
+      </Button>
+    </div>
+  )
+}
+
+export default Avatar
---
+++ src/components/ProfileInformation/ProfileInformation.tsx
@@ -0,0 +1,46 @@
+import { Icon } from 'semantic-ui-react'
+import { Button } from 'decentraland-ui/dist/components/Button/Button'
+import { Profile } from 'decentraland-ui/dist/components/Profile/Profile'
+
+import wallet from '../../assets/icons/Wallet.svg'
+
+import styles from './ProfileInformation.module.css'
+
+const ProfileInformation = () => {
+  return (
+    <div className={styles.ProfileInformation}>
+      {/* TODO: what to do with this size */}
+      <div className={styles.basicRow}>
+        <Profile size="massive" imageOnly address="0xd4fEC88A49EB514e9347eC655D0481D8483a9AE0" />
+        <div className={styles.profileData}>
+          <span className={styles.userNumber}>
+            <span className={styles.userName}>Florencia</span>&nbsp; #222
+          </span>
+          <div className={styles.wallet}>
+            <img src={wallet} />
+            <Profile textOnly address="0xd4fEC88A49EB514e9347eC655D0481D8483a9AE0" />
+            <Icon name="copy" color="grey" />
+          </div>
+          <div>
+            <Icon name="users" />
+            714 friends
+          </div>
+          <span className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+        </div>
+      </div>
+      <div className={styles.actions}>
+        <Button secondary>GET A UNIQUE NAME</Button>
+        <Button secondary className={styles.smallButton}>
+          <Icon name="share alternate"></Icon>
+        </Button>
+        <Button inverted className={styles.smallButton}>
+          <Icon name="ellipsis horizontal"></Icon>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileInformation
