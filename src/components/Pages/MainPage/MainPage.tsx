254ce4e75391835ef829ff3beae50b4187e3b541
Florencia Barreto <florbarrlad@gmail.com>
2023-07-07 18:20:58 +0200

    fix: translations

--- src/components/Pages/MainPage/MainPage.tsx
+++ src/components/Pages/MainPage/MainPage.tsx
@@ -9,7 +9,7 @@ import { Avatar } from '../../Avatar'
 import styles from './MainPage.module.css'
 
 function MainPage() {
-  const tabs: { displayValue: string; value: string }[] = [{ displayValue: t('navigation.overview'), value: t('navigation.overview') }]
+  const tabs: { displayValue: string; value: string }[] = [{ displayValue: t('tabs.overview'), value: t('tabs.overview') }]
 
   const [selectedTab, setSelectedTab] = useState<string>(tabs[0].value)
 
