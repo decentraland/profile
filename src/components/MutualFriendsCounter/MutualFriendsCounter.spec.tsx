daeb097168ba9f6311d692cbb700793d91d78493
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-08-02 17:32:22 -0300

    feat: Mutuals (#80)
    * fix: English translations
    
    * feat: Mutuals
    
    * fix: Add translations
    
    * fix: Add new line

---
+++ src/components/MutualFriendsCounter/MutualFriendsCounter.spec.tsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import { fireEvent } from '@testing-library/react'
+import { renderWithProviders } from '../../tests/tests'
+import MutualFriendsCounter from './MutualFriendsCounter'
+import { Props } from './MutualFriendsCounter.types'
+
+function renderMutualFriendsCounter(props: Partial<Props> = {}) {
+  return renderWithProviders(
+    <MutualFriendsCounter
+      friendAddress={'anAddress'}
+      isLoading={false}
+      count={0}
+      firstMutuals={[]}
+      onFetchMutualFriends={jest.fn()}
+      onClick={jest.fn()}
+      {...props}
+    />
+  )
+}
+
+let renderedComponent: ReturnType<typeof renderMutualFriendsCounter>
+
+describe('when rendering the component with the friends count in zero', () => {
+  beforeEach(() => {
+    renderedComponent = renderMutualFriendsCounter({ count: 0 })
+  })
+
+  it('should render the component with the friends count without the clickable class', async () => {
+    const button = await renderedComponent.getByTestId('mutual-friends-counter')
+    expect(button).not.toHaveClass('clickable')
+    expect(button).toHaveTextContent('0 mutual')
+  })
+})
+
+describe('when rendering the component with the friends count greater than zero', () => {
+  beforeEach(() => {
+    renderedComponent = renderMutualFriendsCounter({ count: 3 })
+  })
+
+  it('should render the component with the friends count without the clickable class', () => {
+    const button = renderedComponent.getByTestId('mutual-friends-counter')
+    expect(button).toHaveClass('clickable')
+    expect(button).toHaveTextContent('3 mutual')
+  })
+})
+
+describe('when clicking the component with a zero count', () => {
+  let onClick: jest.Mock
+
+  beforeEach(() => {
+    onClick = jest.fn()
+    renderedComponent = renderMutualFriendsCounter({ count: 0, onClick })
+    const button = renderedComponent.getByTestId('mutual-friends-counter')
+    fireEvent.click(button)
+  })
+
+  it('should not call the onClick method prop', () => {
+    expect(onClick).not.toHaveBeenCalled()
+  })
+})
+
+describe('when clicking the component with a greater than zero count', () => {
+  let onClick: jest.Mock
+
+  beforeEach(() => {
+    onClick = jest.fn()
+    renderedComponent = renderMutualFriendsCounter({ count: 3, onClick })
+    const button = renderedComponent.getByTestId('mutual-friends-counter')
+    fireEvent.click(button)
+  })
+
+  it('should not call the onClick method prop', () => {
+    expect(onClick).toHaveBeenCalled()
+  })
+})
+
+describe('when rendering the component for the first time', () => {
+  let onFetchMutualFriends: jest.Mock
+
+  beforeEach(() => {
+    onFetchMutualFriends = jest.fn()
+    renderedComponent = renderMutualFriendsCounter({ count: 3, onFetchMutualFriends })
+  })
+
+  it('should call the onFetchMutualFriends method prop', () => {
+    expect(onFetchMutualFriends).toHaveBeenCalled()
+  })
+})
+
+describe('when rendering the component in its loading state', () => {
+  beforeEach(() => {
+    renderedComponent = renderMutualFriendsCounter({ isLoading: true })
+  })
+
+  it('should render the component with a loader', () => {
+    expect(renderedComponent.getByTestId('mutual-friends-counter').querySelector('.loader')).not.toBeNull()
+  })
+})
+
+describe('when rendering the component with the first mutuals', () => {
+  beforeEach(() => {
+    renderedComponent = renderMutualFriendsCounter({ isLoading: true, firstMutuals: ['0x1', '0x2', '0x3'] })
+  })
+
+  it('should render a profile for each of the mutuals', () => {
+    expect(renderedComponent.findByTitle('0x1')).not.toBeNull()
+    expect(renderedComponent.findByTitle('0x2')).not.toBeNull()
+    expect(renderedComponent.findByTitle('0x3')).not.toBeNull()
+  })
+})
---
+++ src/components/MutualFriendsCounter/MutualFriendsCounter.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+import classNames from 'classnames'
+import Profile from 'decentraland-dapps/dist/containers/Profile'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
+import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+import { Props } from './MutualFriendsCounter.types'
+import styles from './MutualFriendsCounter.module.css'
+
+const MutualFriendsCounter = (props: Props) => {
+  const { count, firstMutuals, friendAddress, isLoading, className, onClick, onFetchMutualFriends } = props
+
+  useEffect(() => {
+    onFetchMutualFriends()
+  }, [friendAddress])
+
+  return (
+    <div
+      className={classNames(styles.counter, className, { [styles.clickable]: count > 0 })}
+      role="button"
+      onClick={count > 0 ? onClick : undefined}
+      data-testid="mutual-friends-counter"
+    >
+      {isLoading ? (
+        <Loader active inline size="mini" />
+      ) : (
+        <>
+          {count > 0 ? firstMutuals.map(mutual => <Profile address={mutual} imageOnly />) : null}
+          <span>
+            {count} {t('mutual_friends_counter.mutual')}
+          </span>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default MutualFriendsCounter
