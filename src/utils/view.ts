884aa77a7764732bec666bde5f4554c27050e155
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-08-03 17:27:46 -0300

    refactor: Remove eslint-disable naming-convention

--- src/utils/view.ts
+++ src/utils/view.ts
@@ -1,9 +1,6 @@
 export enum View {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   OWN = 'own',
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   OTHER = 'other'
-  // eslint-disable-next-line @typescript-eslint/naming-convention
 }
 
 export const getView = (loggedInAddress?: string, currentAccountAddress?: string): View => {
