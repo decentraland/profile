5f834587b5f8eadcdcb2720808da1f3ea4ff5689
Florencia Barreto <florbarrlad@gmail.com>
2023-08-03 20:38:38 +0200

    feat: added empty state

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
 import { Item } from '@dcl/schemas'
 import { AssetCardFilters, AssetCard } from 'decentraland-dapps/dist/containers/AssetCard/AssetCard'
+import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+import { Button } from 'decentraland-ui'
+import shirt from '../../assets/images/shirt.svg'
 import { config } from '../../modules/config'
 import { getWearableIds } from './utils'
 import { Props } from './Overview.types'
@@ -10,22 +13,38 @@ import styles from './Overview.module.css'
 const MARKETPLACE_URL = config.get('MARKETPLACE_URL', '')
 
 const Overview = (props: Props) => {
-  const { isLoading, error, items, onFetchItems, profile } = props
+  const { isLoading, items, onFetchItems, profile } = props
+
+  const wratableIds =
+    profile?.avatars[0].avatar.wearables.length &&
+    profile?.avatars[0].avatar.wearables.length > 0 &&
+    getWearableIds(profile?.avatars[0].avatar.wearables)
 
   useEffect(() => {
-    onFetchItems(getWearableIds(profile?.avatars[0].avatar.wearables ?? []))
+    wratableIds && onFetchItems(wratableIds)
   }, [profile])
 
   return (
-    <div className={styles.Overview}>
+    <>
       {isLoading ? (
         <Loader active />
-      ) : error ? (
-        <div>ERROR</div>
+      ) : wratableIds && items.length > 0 ? (
+        <div className={styles.Overview}>
+          {items.map((item: Item) => (
+            <AssetCard asset={item} assetFilters={{} as AssetCardFilters} onClickCardURL={MARKETPLACE_URL} />
+          ))}
+        </div>
       ) : (
-        items.map((item: Item) => <AssetCard asset={item} assetFilters={{} as AssetCardFilters} onClickCardURL={MARKETPLACE_URL} />)
+        <div className={styles.emptyItems}>
+          <img src={shirt} className={styles.emptyIcon} />
+          <span className={styles.title}>{t('overview.start_dressing')}</span>
+          <span>{t('overview.get_collectibles')}</span>
+          <Button inverted as="a" href={MARKETPLACE_URL} target="_blank">
+            {t('overview.go_to_marketplace')}
+          </Button>
+        </div>
       )}
-    </div>
+    </>
   )
 }
 
