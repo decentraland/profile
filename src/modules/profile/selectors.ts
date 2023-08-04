ab8698393b97f00ab5b90e43f4f5884661f456a1
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-08-04 13:38:54 +0200

    feat: Add selectors and tests

--- src/modules/profile/selectors.ts
+++ src/modules/profile/selectors.ts
@@ -1,7 +1,27 @@
 import { createSelector } from '@reduxjs/toolkit'
 import { LOAD_PROFILE_REQUEST } from 'decentraland-dapps/dist/modules/profile/actions'
-import { getLoading } from 'decentraland-dapps/dist/modules/profile/selectors'
+import { getLoading, getProfileOfAddress } from 'decentraland-dapps/dist/modules/profile/selectors'
+import { getAddress } from 'decentraland-dapps/dist/modules/wallet/selectors'
 
 export const isLoadingProfile = createSelector([getLoading, (_state, address) => address], (loadingState, address) =>
   loadingState.some(action => action.type === LOAD_PROFILE_REQUEST && action.payload.address === address.toLowerCase())
+)
+
+const getBlockedAddressesOfProfile = createSelector(
+  [(state, address?: string) => (address ? getProfileOfAddress(state, address) : null)],
+  profile => profile?.avatars[0]?.blocked?.map(address => address.toLowerCase()) ?? []
+)
+
+export const isBlockedByLoggedUser = createSelector(
+  [(_state, profileAddress: string) => profileAddress.toLowerCase(), state => getBlockedAddressesOfProfile(state, getAddress(state))],
+  (addressFromPath, blockedAddresses) => {
+    return blockedAddresses.includes(addressFromPath.toLowerCase())
+  }
+)
+
+export const hasBlockedLoggedUser = createSelector(
+  [getAddress, (state, profileAddress: string) => getBlockedAddressesOfProfile(state, profileAddress)],
+  (loggedAddress, blockedAddresses) => {
+    return !!loggedAddress && blockedAddresses.includes(loggedAddress.toLowerCase())
+  }
 )
