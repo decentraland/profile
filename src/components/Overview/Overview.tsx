5929c9c515b3d4326ec059adaaf3cbaac5a83a63
Florencia Barreto <florbarrlad@gmail.com>
2023-08-04 21:45:21 +0200

    feat: added mobile designs

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
+import classNames from 'classnames'
 import { Item } from '@dcl/schemas'
 import { AssetCardFilters, AssetCard } from 'decentraland-dapps/dist/containers/AssetCard/AssetCard'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+import { useTabletAndBelowMediaQuery } from 'decentraland-ui/dist/components/Media/Media'
 import { Button } from 'decentraland-ui'
 import shirt from '../../assets/images/shirt.svg'
 import { config } from '../../modules/config'
@@ -13,6 +15,8 @@ const MARKETPLACE_URL = config.get('MARKETPLACE_URL', '')
 
 const Overview = (props: Props) => {
   const { isLoading, items, onFetchItems, wearableIds } = props
+
+  const isTabletAndBelow = useTabletAndBelowMediaQuery()
 
   useEffect(() => {
     if (wearableIds.length > 0) {
@@ -25,15 +29,17 @@ const Overview = (props: Props) => {
       {isLoading ? (
         <Loader active />
       ) : items.length > 0 ? (
-        <div className={styles.Overview}>
+        <div className={classNames(styles.Overview, isTabletAndBelow && styles.OverviewMobile)}>
           {items.map((item: Item) => (
             <a href={`${MARKETPLACE_URL}${item.url}`} rel="noopener noreferrer" target="_blank">
               <AssetCard asset={item} assetFilters={{} as AssetCardFilters} onClickCardURL="" />
             </a>
           ))}
+          {/* TODO: remove this test card */}
+          <AssetCard asset={items[0]} assetFilters={{} as AssetCardFilters} onClickCardURL="" />
         </div>
       ) : (
-        <div className={styles.emptyItems} data-testid="overview-empty">
+        <div className={classNames(styles.emptyItems, isTabletAndBelow && styles.emptyItemsMobile)} data-testid="overview-empty">
           <img src={shirt} className={styles.emptyIcon} />
           <span className={styles.title}>{t('overview.start_dressing')}</span>
           <span>{t('overview.get_collectibles')}</span>
--- src/components/ProfileInformation/ProfileInformation.tsx
+++ src/components/ProfileInformation/ProfileInformation.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback } from 'react'
+import classnames from 'classnames'
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon/Icon'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Button } from 'decentraland-ui/dist/components/Button/Button'
 import { Dropdown } from 'decentraland-ui/dist/components/Dropdown/Dropdown'
+import { useTabletAndBelowMediaQuery } from 'decentraland-ui/dist/components/Media/Media'
 import { Profile } from 'decentraland-ui/dist/components/Profile/Profile'
 import Share from '../../assets/icons/Share.svg'
 import Twitter from '../../assets/icons/Twitter.svg'
@@ -26,11 +28,14 @@ import styles from './ProfileInformation.module.css'
 const EXPLORER_URL = config.get('EXPLORER_URL', '')
 const PROFILE_URL = config.get('PROFILE_URL', '')
 const ADDRESS_SHORTENED_LENGTH = 24
+const ADDRESS_SHORTENED_LENGTH_MOBILE = 8
 
 const ProfileInformation = (props: Props) => {
   const { profile, isSocialClientReady, loggedInAddress, profileAddress, onViewMore } = props
 
   const [hasCopiedAddress, setHasCopied] = useTimer(1200)
+
+  const isTabletAndBelow = useTabletAndBelowMediaQuery()
 
   const avatar = profile?.avatars[0]
 
@@ -49,29 +54,39 @@ const ProfileInformation = (props: Props) => {
   const shouldShowViewMoreButton = hasAboutInformation(avatar)
 
   return (
-    <div className={styles.ProfileInformation}>
-      <div className={styles.basicRow}>
-        <Profile size="massive" imageOnly address={profileAddress} avatar={avatar} />
-        <div className={styles.avatar}>
+    <div className={classnames(styles.ProfileInformation, isTabletAndBelow && styles.ProfileInformationMobile)}>
+      <div className={classnames(styles.basicRow, isTabletAndBelow && styles.basicColumn)}>
+        <Profile size="massive" className={styles.avatar} imageOnly address={profileAddress} avatar={avatar} />
+        <div className={styles.avatarInformation}>
           <span className={styles.userNumber}>
             <span className={styles.userName} data-testid={profileAddress}>
               {avatarName.name}
             </span>
             {avatarName.lastPart ? <span>&nbsp; {avatarName.lastPart}</span> : null}
           </span>
-          <div className={styles.wallet}>
-            <img src={Wallet} className={styles.walletIcon} />
-            {profileAddress.slice(0, ADDRESS_SHORTENED_LENGTH)}...
-            <Button basic onClick={handleCopyLink} className={styles.copyLink}>
-              <CopyIcon />
-            </Button>
-          </div>
-          {isSocialClientReady && (
-            <div className={styles.basicCenteredRow}>
-              {isLoggedInProfile ? <FriendsCounter /> : <MutualFriendsCounter friendAddress={profileAddress} />}
+          <div className={classnames(styles.column, isTabletAndBelow && styles.reverseColumnInformation)}>
+            <div className={styles.wallet}>
+              <img src={Wallet} className={styles.walletIcon} />
+              {profileAddress.slice(0, isTabletAndBelow ? ADDRESS_SHORTENED_LENGTH_MOBILE : ADDRESS_SHORTENED_LENGTH)}...
+              <Button basic onClick={handleCopyLink} className={styles.copyLink}>
+                <CopyIcon />
+              </Button>
             </div>
+            {isSocialClientReady && (
+              <div className={styles.basicCenteredRow}>
+                {isLoggedInProfile ? <FriendsCounter /> : <MutualFriendsCounter friendAddress={profileAddress} />}
+              </div>
+            )}
+          </div>
+
+          {avatar && (
+            <span className={styles.description}>
+              {/* TODO: remove the lorem ipsum test message  */}
+              "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ull"
+              {avatar.description}
+            </span>
           )}
-          {avatar && <span className={styles.description}>{avatar.description}</span>}
           {shouldShowViewMoreButton && (
             <div className={styles.basicCenteredRow}>
               <Button basic className={styles.viewMore} onClick={handleViewMore}>
@@ -81,8 +96,8 @@ const ProfileInformation = (props: Props) => {
           )}
         </div>
       </div>
-      <div className={styles.actions}>
-        <div className={styles.buttons}>
+      <div className={classnames(styles.actions, isTabletAndBelow && styles.actionsMobile)}>
+        <div className={classnames(styles.buttons, isTabletAndBelow && styles.buttonsMobile)}>
           {shouldShowFriendsButton ? <FriendshipButton friendAddress={profileAddress} /> : null}
           {loggedInAddress ? <WorldsButton isLoggedIn={isLoggedInProfile} address={profileAddress} /> : null}
           <Dropdown
