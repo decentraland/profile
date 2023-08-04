ca8d92cde450df607498958dc552a941543b7ebf
Florencia Barreto <32873485+flobarreto@users.noreply.github.com>
2023-08-04 23:39:17 +0200

    feat: added mobile designs (#96)
    * feat: added mobile designs
    
    * feat: removed todos
    
    * fix: build
    
    * fix: build
    
    * fix: build
    
    * fix: mainpage blocked
    
    * feat: removed worlds button for mobile
    
    * fix: Small fixes
    
    * fix: Reduced blur a bit
    
    * fix: Missing newline
    
    * fix: Add missing newline
    
    * fix: Add missing newline
    
    ---------
    
    Co-authored-by: Lautaro Petaccio <lausuper@gmail.com>

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react'
+import classNames from 'classnames'
 import { Item } from '@dcl/schemas'
 import { AssetCardFilters, AssetCard } from 'decentraland-dapps/dist/containers/AssetCard/AssetCard'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
 import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+import { useTabletAndBelowMediaQuery } from 'decentraland-ui/dist/components/Media/Media'
 import { Button } from 'decentraland-ui'
 import shirt from '../../assets/images/shirt.svg'
 import { config } from '../../modules/config'
@@ -12,7 +14,9 @@ import styles from './Overview.module.css'
 const MARKETPLACE_URL = config.get('MARKETPLACE_URL', '')
 
 const Overview = (props: Props) => {
-  const { isLoading, items, onFetchItems, wearableIds } = props
+  const { isLoading, items, onFetchItems, wearableIds, className } = props
+
+  const isTabletAndBelow = useTabletAndBelowMediaQuery()
 
   useEffect(() => {
     if (wearableIds.length > 0) {
@@ -21,11 +25,11 @@ const Overview = (props: Props) => {
   }, [wearableIds])
 
   return (
-    <>
+    <div className={classNames(className)}>
       {isLoading ? (
         <Loader active />
       ) : items.length > 0 ? (
-        <div className={styles.Overview}>
+        <div className={classNames(styles.Overview, isTabletAndBelow && styles.OverviewMobile)}>
           {items.map((item: Item) => (
             <a href={`${MARKETPLACE_URL}${item.url}`} rel="noopener noreferrer" target="_blank" key={item.id}>
               <AssetCard asset={item} assetFilters={{} as AssetCardFilters} />
@@ -33,7 +37,7 @@ const Overview = (props: Props) => {
           ))}
         </div>
       ) : (
-        <div className={styles.emptyItems} data-testid="overview-empty">
+        <div className={classNames(styles.emptyItems, isTabletAndBelow && styles.emptyItemsMobile)} data-testid="overview-empty">
           <img src={shirt} className={styles.emptyIcon} />
           <span className={styles.title}>{t('overview.start_dressing')}</span>
           <span>{t('overview.get_collectibles')}</span>
@@ -42,7 +46,7 @@ const Overview = (props: Props) => {
           </Button>
         </div>
       )}
-    </>
+    </div>
   )
 }
 
--- src/components/Pages/MainPage/MainPage.tsx
+++ src/components/Pages/MainPage/MainPage.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import classNames from 'classnames'
 import Divider from 'semantic-ui-react/dist/commonjs/elements/Divider/Divider'
 import { t } from 'decentraland-dapps/dist/modules/translation/utils'
-import { Tabs } from 'decentraland-ui/dist/components/Tabs/Tabs'
-import { Loader } from 'decentraland-ui'
+import { Loader } from 'decentraland-ui/dist/components/Loader/Loader'
+import { useTabletAndBelowMediaQuery } from 'decentraland-ui/dist/components/Media/Media'
 import { locations } from '../../../modules/routing/locations'
 import { getView } from '../../../utils/view'
 import { Avatar } from '../../Avatar'
@@ -17,17 +18,20 @@ import styles from './MainPage.module.css'
 
 function MainPage(props: Props) {
   const { isLoading, profileAddress, loggedInAddress, isBlocked, onFetchProfile } = props
-  const tabs: { displayValue: string; value: string }[] = [{ displayValue: t('tabs.overview'), value: t('tabs.overview') }]
   const view = getView(loggedInAddress, profileAddress)
 
-  const [selectedTab, setSelectedTab] = useState<string>(tabs[0].value)
+  const isTabletAndBelow = useTabletAndBelowMediaQuery()
 
-  const handleTabChange = useCallback(
-    (tab: string) => {
-      setSelectedTab(tab)
-    },
-    [setSelectedTab]
-  )
+  //  TODO: use this on tabs implementation
+  // const tabs: { displayValue: string; value: string }[] = [{ displayValue: t('tabs.overview'), value: t('tabs.overview') }]
+  // const [selectedTab, setSelectedTab] = useState<string>(tabs[0].value)
+  // const handleTabChange = useCallback(
+  //   (tab: string) => {
+  //     setSelectedTab(tab)
+  //   },
+  //   [setSelectedTab]
+  // )
+
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -45,23 +49,28 @@ function MainPage(props: Props) {
   return (
     <PageLayout>
       {isLoading ? (
-        <Loader active />
+        <div className={styles.fullPage}>
+          <Loader active />
+        </div>
       ) : (
-        <div className={styles.MainPage}>
-          {selectedTab === tabs[0].value && <Avatar view={view} profileAddress={profileAddress ?? loggedInAddress ?? nullAddress} />}
-          <div className={styles.infoContainer}>
+        <div className={classNames(styles.MainPage, isTabletAndBelow && styles.MainPageMobile)}>
+          {/* TODO: use this on tabs implementation
+          {selectedTab === tabs[0].value && <Avatar view={view} profileAddress={profileAddress ?? loggedInAddress ?? nullAddress} />} */}
+          <Avatar view={view} profileAddress={profileAddress ?? loggedInAddress ?? nullAddress} />
+          <div className={classNames(styles.infoContainer, isTabletAndBelow && styles.infoContainerMobile)}>
             <ProfileInformation profileAddress={profileAddress ?? loggedInAddress ?? nullAddress} loggedInAddress={loggedInAddress} />
             <Divider />
             {!isBlocked && (
               <div data-testid="TBD">
-                <Tabs>
+                {/* <Tabs>
                   {tabs.map(tab => (
                     <Tabs.Tab key={tab.value} active={selectedTab === tab.value} onClick={() => handleTabChange(tab.value)}>
                       <span className={styles.tab}>{tab.displayValue}</span>
                     </Tabs.Tab>
                   ))}
-                </Tabs>
-                <Overview profileAddress={profileAddress ?? loggedInAddress ?? ''} />
+                </Tabs> */}
+                <span className={styles.tabTitle}>{t('overview.title')}</span>
+                <Overview className={styles.overview} profileAddress={profileAddress ?? loggedInAddress ?? ''} />
               </div>
             )}
             {isBlocked && (
