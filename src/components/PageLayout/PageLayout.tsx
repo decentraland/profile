e499c2bc3febbe67a405f7efbe85e47cb15cb33f
Florencia Barreto <florbarrlad@gmail.com>
2023-08-09 19:47:31 +0200

    fix: scroll behaviour

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import classNames from 'classnames'
+import { useTabletAndBelowMediaQuery } from 'decentraland-ui/dist/components/Media/Media'
 import { Footer } from '../Footer'
 import Navbar from '../Navbar'
 import { Props } from './PageLayout.types'
 import styles from './PageLayout.module.css'
 
 const PageLayout = ({ children, className }: Props) => {
+  const isTabletAndBelow = useTabletAndBelowMediaQuery()
+
   return (
-    <div className={classNames(styles.page, className)}>
+    <div className={classNames(styles.page, className, isTabletAndBelow && styles.pageMobile)}>
       <Navbar className={styles.navbar} isFullscreen />
-      <div className={styles.content}>{children}</div>
+      <div className={classNames(styles.content, isTabletAndBelow && styles.contentMobile)}>{children}</div>
       <Footer className={styles.footer} />
     </div>
   )
