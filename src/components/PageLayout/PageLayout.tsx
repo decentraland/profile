40fabdb5a8a53bad8ef35e7821cbe1e4ec69258b
Florencia Barreto <florbarrlad@gmail.com>
2023-08-10 15:25:49 +0200

    fix: comments

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import classNames from 'classnames'
-import { useTabletAndBelowMediaQuery } from 'decentraland-ui/dist/components/Media/Media'
 import { Footer } from '../Footer'
 import Navbar from '../Navbar'
 import { Props } from './PageLayout.types'
 import styles from './PageLayout.module.css'
 
 const PageLayout = ({ children, className }: Props) => {
-  const isTabletAndBelow = useTabletAndBelowMediaQuery()
-
   return (
-    <div className={classNames(styles.page, className, isTabletAndBelow && styles.pageMobile)}>
+    <div className={classNames(styles.page, className)}>
       <Navbar className={styles.navbar} isFullscreen />
-      <div className={classNames(styles.content, isTabletAndBelow && styles.contentMobile)}>{children}</div>
+      <div className={styles.content}>{children}</div>
       <Footer className={styles.footer} />
     </div>
   )
