011955990d7edd767ab754f91ac1abca8b631a1d
Lautaro Petaccio <lausuper@gmail.com>
2023-08-23 10:03:48 -0300

    fix: Remove node-fetch

--- src/tests/beforeSetupTests.tsx
+++ src/tests/beforeSetupTests.tsx
@@ -6,7 +6,6 @@ import path from 'path'
 import { TextEncoder, TextDecoder } from 'util'
 import { config } from 'dotenv'
 import flatten from 'flat'
-import fetch, { Request, Response } from 'node-fetch'
 import { en as dappsEn } from 'decentraland-dapps/dist/modules/translation/defaults'
 import { mergeTranslations, setCurrentLocale } from 'decentraland-dapps/dist/modules/translation/utils'
 import * as locales from '../modules/translation/locales'
@@ -26,10 +25,6 @@ Object.assign(globalThis, { TextDecoder, TextEncoder })
 setCurrentLocale('en', mergeTranslations(flatten(dappsEn), flatten(locales.en)))
 
 if (!globalThis.fetch) {
-  // @ts-expect-error replaces the fetch function for the node-fetch one to support fetch in NodeJS
-  globalThis.fetch = fetch
-  // @ts-expect-error replaces the Request class for the node-fetch one to support fetch in NodeJS
-  globalThis.Request = Request
-  // @ts-expect-error replaces the Response component for the node-fetch one to support fetch in NodeJS
-  globalThis.Response = Response
+  globalThis.fetch = jest.fn()
+  globalThis.Request = jest.fn()
 }
