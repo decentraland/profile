edd19aea8d2eda165a6657936ab967fe67ce5519
Kevin Szuchet <kevin.szuchet@decentraland.org>
2023-09-06 14:41:41 +0200

    fix: Avoid not allowed chars in the release name

--- src/modules/analytics/sentry.ts
+++ src/modules/analytics/sentry.ts
@@ -3,7 +3,7 @@ import { config } from '../config'
 
 init({
   environment: config.get('ENVIRONMENT'),
-  release: `${process.env.REACT_APP_WEBSITE_NAME}@${process.env.REACT_APP_WEBSITE_VERSION}`,
+  release: `${config.get('SENTRY_RELEASE_PREFIX', 'profile')}@${process.env.REACT_APP_WEBSITE_VERSION}`,
   dsn: config.get('SENTRY_DSN'),
   integrations: [new BrowserTracing(), new Replay()],
   // Performance Monitoring
