421ede5b8b8afd567be68d87506092bdb7615b5d
Nando <24811313+fzavalia@users.noreply.github.com>
2023-09-06 11:47:07 -0300

    fix: Lint

--- src/components/HOC/WithPageView/WithPageView.tsx
+++ src/components/HOC/WithPageView/WithPageView.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import { getAnalytics } from 'decentraland-dapps/dist/modules/analytics/utils'
 import { useLocation } from 'react-router-dom'
+import { getAnalytics } from 'decentraland-dapps/dist/modules/analytics/utils'
 import { Props } from './WithPageView.types'
 
 const WithPageView = ({ children }: Props) => {
