e93ce018b4df7959c2b6672070c799ff498c046b
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-09-18 14:12:41 -0300

    fix: Overview tests (#95)
    * fix: Overview tests
    
    * fix: Add README and coverage
    
    * fix: Add coveralls
    
    * fix: Remove coveralls

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# profile
-Profile dApp
+<p align="center">
+  <a href="https://decentraland.org">
+    <img alt="Decentraland" src="https://decentraland.org/images/logo.png" width="60" />
+  </a>
+</p>
+<h1 align="center">
+  Decentraland Profile UI
+</h1>
+
+The Decentraland Profile UI repository contains the UI code to belonging to the https://profile.decentraland.org site.
--- src/components/Overview/Overview.spec.tsx
+++ src/components/Overview/Overview.spec.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BodyShape, Item, NFTCategory, Network, Rarity, WearableCategory } from '@dcl/schemas'
 import { renderWithProviders } from '../../tests/tests'
 import Overview from './Overview'
 import { Props } from './Overview.types'
@@ -49,5 +50,61 @@ describe('when rendering the component without items', () => {
 
   it('should render the component with a message', () => {
     expect(renderedComponent.getByTestId('overview-empty')).toBeInTheDocument()
+  })
+})
+
+describe('when rendering the component as loading', () => {
+  beforeEach(() => {
+    renderedComponent = renderOverview({ isLoading: true })
+  })
+
+  it('should render the component with a spinner', () => {
+    expect(renderedComponent.queryByTestId('overview')?.querySelector('.loader')).not.toBeNull()
+  })
+})
+
+describe('when rendering the component with items', () => {
+  let items: Item[]
+
+  beforeEach(() => {
+    items = [
+      {
+        id: '1',
+        name: 'a name',
+        thumbnail: 'a thumbnail',
+        url: 'a url',
+        rarity: Rarity.COMMON,
+        price: '100',
+        beneficiary: 'a beneficiary',
+        contractAddress: 'a contract address',
+        isOnSale: true,
+        category: NFTCategory.WEARABLE,
+        itemId: 'anId',
+        available: 1,
+        creator: '0x0',
+        createdAt: 10123123,
+        updatedAt: 10123123,
+        reviewedAt: 10123123,
+        soldAt: 10123123,
+        network: Network.ETHEREUM,
+        chainId: 1,
+        data: {
+          wearable: {
+            description: 'a description',
+            category: WearableCategory.EARRING,
+            bodyShapes: [BodyShape.MALE],
+            rarity: Rarity.COMMON,
+            isSmart: false
+          }
+        },
+        urn: 'anUrn',
+        firstListedAt: 10123123
+      }
+    ]
+    renderedComponent = renderOverview({ items })
+  })
+
+  it('should render an asset card for each item', () => {
+    expect(renderedComponent.getByTestId(`overview-${items[0].id}`)).toBeInTheDocument()
   })
 })
