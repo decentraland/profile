39acb30aaa85ae9cb2964e5dcd8079c2cceb1a96
Lautaro Petaccio <lausuper@gmail.com>
2023-10-03 15:31:25 -0300

    fix: FiltersModal test

---
+++ src/components/Modals/FiltersModal/constants.ts
@@ -0,0 +1,3 @@
+export const FILTERS_MODAL_CLOSE_BUTTON_DATA_TEST_ID = 'filters-modal-close-button-data-test-id'
+export const FILTERS_MODAL_APPLY_FILTERS_BUTTON_DATA_TEST_ID = 'filters-modal-apply-filters-button-data-test-id'
+export const FILTERS_MODAL_CLEAR_FILTERS_BUTTON_DATA_TEST_ID = 'filters-modal-clear-filters-button-data-test-id'
