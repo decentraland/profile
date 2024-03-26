821aef002a67f849211c7947b8be273dfab41cd4
Melisa Anabella Rossi <melisa.rossi@decentraland.org>
2024-03-25 22:54:15 -0300

    feat: add notifications

--- src/components/Navbar/Navbar.container.ts
+++ src/components/Navbar/Navbar.container.ts
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux'
 import { getPendingTransactions } from 'decentraland-dapps/dist/modules/transaction/selectors'
 import { getAddress, isConnected } from 'decentraland-dapps/dist/modules/wallet/selectors'
+import { getCurrentIdentity } from '../../modules/identity/selector'
 import { RootState } from '../../modules/reducer'
 import Navbar from './Navbar'
 import { MapStateProps } from './Navbar.types'
@@ -9,7 +10,8 @@ const mapState = (state: RootState): MapStateProps => {
   const address = getAddress(state)
   return {
     hasActivity: address ? getPendingTransactions(state, address).length > 0 : false,
-    isConnected: isConnected(state)
+    isConnected: isConnected(state),
+    identity: getCurrentIdentity(state) || undefined
   }
 }
 
