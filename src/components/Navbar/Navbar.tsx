23040f27d51ae79e9e2829b28b21e472c4716ddc
Gabriel Díaz <gabrieldiaz31@gmail.com>
2024-12-03 17:24:42 -0300

    fix: Auth redirection (#350)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,20 @@
 import React, { useCallback } from 'react'
+import { useLocation } from 'react-router-dom'
 import { Navbar as BaseNavbar } from 'decentraland-dapps/dist/containers'
 import { config } from '../../modules/config'
 import { Props } from './Navbar.types'
 import './Navbar.css'
 
-const AUTH_URL = config.get('AUTH_URL')
-
 const Navbar = (props: Props) => {
+  const { pathname, search } = useLocation()
+
   const handleSignIn = useCallback(() => {
-    window.location.replace(`${AUTH_URL}?redirectTo=${window.location.href}`)
+    const searchParams = new URLSearchParams(search)
+    const currentRedirectTo = searchParams.get('redirectTo')
+    const basename = /^decentraland.(zone|org|today)$/.test(window.location.host) ? '/profile' : ''
+    const redirectTo = !currentRedirectTo ? `${basename}${pathname}${search}` : `${basename}${currentRedirectTo}`
+
+    window.location.replace(`${config.get('AUTH_URL')}/login?redirectTo=${redirectTo}`)
   }, [])
 
   return <BaseNavbar {...props} onSignIn={handleSignIn} withNotifications />
