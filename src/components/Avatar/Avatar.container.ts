7e18c8080ccd30c97399118f7efeb67d15b984c8
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-06-19 10:48:19 -0300

    feat: pass new params unity and unityMode to the WerablePreview

--- src/components/Avatar/Avatar.container.ts
+++ src/components/Avatar/Avatar.container.ts
@@ -1,11 +1,15 @@
 import { connect } from 'react-redux'
 import { getProfileOfAddress } from 'decentraland-dapps/dist/modules/profile/selectors'
+import { getIsUnityWearablePreviewEnabled } from '../../modules/features/selectors'
 import { RootState } from '../../modules/reducer'
 import Avatar from './Avatar'
 import { MapStateProps, OwnProps } from './Avatar.types'
 
 const mapState = (state: RootState, ownProps: OwnProps): MapStateProps => {
-  return { profile: getProfileOfAddress(state, ownProps.profileAddress) }
+  return {
+    profile: getProfileOfAddress(state, ownProps.profileAddress),
+    isUnityWearablePreviewEnabled: getIsUnityWearablePreviewEnabled(state)
+  }
 }
 
 export default connect(mapState)(Avatar)
