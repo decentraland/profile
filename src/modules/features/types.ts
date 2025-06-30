fa98594b2a51077882d99a446328c06e5be9a24a
Braian Mellor <braianj@gmail.com>
2025-06-30 17:12:30 -0300

    fix: feature flage name

--- src/modules/features/types.ts
+++ src/modules/features/types.ts
@@ -2,5 +2,5 @@ export enum FeatureName {
   AUTH_DAPP = 'auth-dapp',
   NAVBAR_UI2 = 'navbar-ui2',
   REFERRAL = 'show-referral',
-  REFERRAL_TESTING_BUTTON = 'dapps-show-referral-testing-button'
+  REFERRAL_TESTING_BUTTON = 'show-referral-testing-button'
 }
