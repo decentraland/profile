75304db9b1b15f107854550873e867450cf8b1d7
Braian Mellor <braianj@gmail.com>
2025-07-10 11:52:43 -0300

    test: update

--- src/components/Referrals/Referrals.spec.tsx
+++ src/components/Referrals/Referrals.spec.tsx
@@ -9,8 +9,10 @@ jest.mock('./ReferralHeroSection', () => ({
   ReferralHeroSection: () => <div data-testid={MOCK_HERO_SECTION_TEST_ID} />
 }))
 
-jest.mock('./ReferralJourney', () => ({
-  ReferralJourney: () => <div data-testid={MOCK_JOURNEY_TEST_ID} />
+jest.mock('./ReferralJourney/ReferralJourney.container', () => ({
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  __esModule: true,
+  default: () => <div data-testid={MOCK_JOURNEY_TEST_ID} />
 }))
 
 describe('Referrals', () => {
