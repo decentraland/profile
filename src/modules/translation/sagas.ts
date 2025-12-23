b15e49b0ef3306c19e98688e8381c645919d8ff5
Braian Mellor <braianj@gmail.com>
2025-12-23 12:34:44 -0300

    fix: remove non-existent Locale type import

--- src/modules/translation/sagas.ts
+++ src/modules/translation/sagas.ts
@@ -3,7 +3,6 @@ import { STORAGE_LOAD } from 'decentraland-dapps/dist/modules/storage/actions'
 import { fetchTranslationsRequest } from 'decentraland-dapps/dist/modules/translation/actions'
 import { createTranslationSaga } from 'decentraland-dapps/dist/modules/translation/sagas'
 import { getLocale } from 'decentraland-dapps/dist/modules/translation/selectors'
-import { Locale } from 'decentraland-dapps/dist/modules/translation/types'
 import { getPreferredLocale } from 'decentraland-dapps/dist/modules/translation/utils'
 import * as translations from './locales'
 
@@ -11,10 +10,10 @@ const baseTranslationSaga = createTranslationSaga({
   translations
 })
 
-const locales = Object.keys(translations) as Locale[]
+const locales = Object.keys(translations)
 
-function* handleStorageLoad() {
-  const localeInState: Locale | undefined = yield select(getLocale)
+function* handleStorageLoad(): Generator {
+  const localeInState: string | undefined = yield select(getLocale)
   const locale = localeInState || getPreferredLocale(locales) || locales[0]
   yield put(fetchTranslationsRequest(locale))
 }
