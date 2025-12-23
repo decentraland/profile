d21d25ba9eee7418c86edc076bab14651f310f9e
Braian Mellor <braianj@gmail.com>
2025-12-23 14:19:35 -0300

    feat: revert changes in translation sagas

--- src/modules/translation/sagas.ts
+++ src/modules/translation/sagas.ts
@@ -1,24 +1,6 @@
-import { all, fork, put, select, takeEvery } from 'redux-saga/effects'
-import { STORAGE_LOAD } from 'decentraland-dapps/dist/modules/storage/actions'
-import { fetchTranslationsRequest } from 'decentraland-dapps/dist/modules/translation/actions'
 import { createTranslationSaga } from 'decentraland-dapps/dist/modules/translation/sagas'
-import { getLocale } from 'decentraland-dapps/dist/modules/translation/selectors'
-import { getPreferredLocale } from 'decentraland-dapps/dist/modules/translation/utils'
-import { Locale } from 'decentraland-ui/dist/components/Language/Language'
 import * as translations from './locales'
 
-const baseTranslationSaga = createTranslationSaga({
+export const translationSaga = createTranslationSaga({
   translations
 })
-
-const locales = Object.keys(translations) as Locale[]
-
-function* handleStorageLoad(): Generator {
-  const localeInState: Locale | undefined = yield select(getLocale)
-  const locale = localeInState || getPreferredLocale(locales) || locales[0]
-  yield put(fetchTranslationsRequest(locale))
-}
-
-export function* translationSaga() {
-  yield all([fork(baseTranslationSaga), takeEvery(STORAGE_LOAD, handleStorageLoad)])
-}
